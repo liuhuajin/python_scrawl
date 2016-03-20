7193d0479549dc81ce66a9822c115bf7c44c4baa
liuhuajin <liuhuajinbenton@163.com>
2016-03-20 23:16:34 +0800

    download image from certain question OO

--- zhihu.py
+++ zhihu.py
@@ -1,22 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 
-s = requests.session()
-login_data = {'email':'liuhuajinbenton@163.com', 'password':'7443923555'}
-s.post('http://www.zhihu.com/login', login_data)
-r = s.get('http://zhihu.com/question/23978389')
-content = r.content
-soup = BeautifulSoup(content, 'lxml')
-images = soup.find_all('img')
-i = 0
-for image in images:
-	image_path = image.get('data-original')
-	if image_path:
-		image_response = requests.get(image_path)
-		print image_response.status_code
-		image_content = image_response.content
-		if image_content:
-			with(open('./image/image_'+str(i)+'.jpg', 'wb')) as f:
-				f.write(image_content)
-			print 'write iamge done, %s' %i
+
+
+
+class image_catcher(object):
+	def __init__(self, email, password):
+		self.email = email
+		self.password = password
+		self.s = None
+		self.s = self.login_user()
+	
+	 def login_user():
+		s = requests.session()
+		login_data = {'email': self.email, 'password': self.password}
+		s.post('http://www.zhihu.com/login', login_data)
+		return s
+	
+	def get_image_contents(url):
+		r = self.s.get(url)
+		content = r.content
+		soup = BeautifulSoup(content, 'lxml')
+		images = soup.find_all('img')
+		i = 0
+		for image in images:
+			image_path = image.get('data-original')
+			name = './image/' + str(i) + '.jpg'
+			save_image(image_path, name)
 			i += 1
+
+	def save_image(image_path, name):
+		if image_path:
+			image_response = requests.get(image_path)
+			if not image_response.status == 200: return
+			image_content = image_response.content
+			if image_content and i%2 == 0:
+				with(open(name, 'wb')) as f:
+					f.write(image_content)
+				print 'write iamge done, %s' %name
