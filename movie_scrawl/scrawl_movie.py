d8eeb6d57236e32219311f543010edf21f751f5c
liuhuajin <liuhuajinbenton@163.com>
2016-03-28 23:22:21 +0800

    first part

--- movie_scrawl/scrawl_movie.py
+++ movie_scrawl/scrawl_movie.py
@@ -1,8 +1,21 @@
 import requests
 
 class MovieScrawl(object):
-	def __init__(self):
-		pass
+	def __init__(self, url, pages):
+		self.start_url = url
+		self.total_pages = pages
 	
-	def set_init_data(self):
-		pass
+	def scrawl_url(self, url):
+		try:
+			print 'start scrawl %s'%surl
+			url_response = requests.get(url, timeout=5)
+			
+		except:
+			pass
+	
+	def start_scrawl(self):
+		pool = threadpool.ThreadPool(8)
+		urls = [self.start_url+str(page) for page in xrange(1, self.total_pages+1)]
+		reqs = threadpool.makeRequests(lambda url:self.scrawl_url(url), urls)
+		[pool.putrequest(req) for req in reqs]
+		pool.wait()
