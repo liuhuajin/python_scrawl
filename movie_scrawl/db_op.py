a783ad3f3498dc7d3625ba321673d246dc4309be
liuhj <liuhuajinbenton@163.com>
2016-04-06 20:16:49 +0800

    db singleton

--- movie_scrawl/db_op.py
+++ movie_scrawl/db_op.py
@@ -1,11 +1,19 @@
 #-*- encoding:utf-8 -*-
 from pymongo import MongoClient
 
+def singleton(cls, *args, **kw):
+	instances = {}
+	def _singleton():
+		if cls not in instances:
+			instances[cls] = cls(*args, **kw)
+		return instances[cls]
+	return _singleton
+
+@singleton
 class DBOperation(object):
 	def __init__(self, host, port):
-		self.host = host
-		self.prot = port
-		self.client = None
-
-	def get_client(self):
-		self.client = MongoClient('localhost', 3000)
+		self._host = host
+		self._prot = port
+		self._db_name = dbname
+		self._client = MongoClient(self._host, self._port)
+		self.db = getattr(self._client, self._db_name)
