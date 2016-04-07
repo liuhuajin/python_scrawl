602831552a83f1adbfda37c8f70f61726598a641
liuhuajin <liuhuajinbenton@163.com>
2016-04-07 22:02:35 +0800

    db singleton

--- movie_scrawl/db_op.py
+++ movie_scrawl/db_op.py
@@ -12,11 +12,17 @@ def singleton(cls, *args, **kw):
 
 @singleton
 class DBOperation(object):
-	def __init__(self, host, port):
+	def __init__(self):
 		conf = ConfigParser.ConfigParser()
 		conf.read('pro.cfg')
 		self._host = conf.get('db_config', 'host')
-		self._prot = int(conf.get('db_config', 'host'))
+		self._port = int(conf.get('db_config', 'port'))
 		self._db_name =  conf.get('db_config', 'db_name')
 		self._client = MongoClient(self._host, self._port)
 		self.db = getattr(self._client, self._db_name)
+
+if __name__ == '__main__':
+	db_1 = DBOperation()
+	db_2 = DBOperation()
+	print id(db_1)
+	print id(db_2)
